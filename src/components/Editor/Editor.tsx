ec188962dc04ed87726d295ce433138c90488e95
MuhammadRadifa <m.difa123456@gmail.com>
2023-10-04 14:41:43 +0800

    🚀feat : add new hover feature to editor

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,15 +1,17 @@
 import React, { useCallback, useState } from 'react';
 import './style.css';
 import { IStyle } from '@site/src/utils/interfaces/style';
-import { useUnitFilter } from '@site/src/utils/hooks/useUnitFilter';
+import { useUnitFilter } from '@site/src/utils/hooks';
 import { editorInput } from '@site/src/constant';
 import { IconContext } from 'react-icons';
 import { jsonToCss, borderMerge, shadowOpacity } from '@site/src/utils/service';
 
-export function Editor({ element, initStyle, initContent }) {
+export function Editor({ element, initStyle, initHover, initContent }) {
   const [width, setWidth] = useState(70);
   const [editorView, setEditorView] = useState<string>(Object.keys(initStyle)[0]);
+  const [isHover, setIsHover] = useState<boolean>(false);
   const [style, setStyle] = useState<IStyle>(initStyle);
+  const [styleStorage, setStyleStorage] = useState<IStyle>(initHover);
   const [content, setContent] = useState(initContent);
   const [propertyShow, setPropertyShow] = useState({
     border: initStyle[editorView].hasOwnProperty('borderWidth'),
@@ -71,11 +73,28 @@ export function Editor({ element, initStyle, initContent }) {
     [content],
   );
 
-  const handleShowProperty = useCallback((e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
-    const property = e.currentTarget.id;
-    if (property === 'border') {
-    }
-  }, []);
+  const handleShowProperty = useCallback(
+    (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
+      const property = e.currentTarget.id.toLowerCase();
+      setPropertyShow({ ...propertyShow, [property]: !propertyShow[property] });
+      if (property === 'border') {
+        if (!style[editorView]['borderWidth']) {
+          setStyle({
+            ...style,
+            [editorView]: { ...style[editorView], borderWidth: '10px', borderColor: 'black', borderStyle: 'solid' },
+          });
+        }
+      } else if (property === 'shadow') {
+        if (!style[editorView]['boxShadow']) {
+          setStyle({
+            ...style,
+            [editorView]: { ...style[editorView], boxShadow: '6px 10px 5px 0px rgba(188,188,188,0.75)' },
+          });
+        }
+      }
+    },
+    [propertyShow],
+  );
 
   const handleEditorView = useCallback((e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.stopPropagation();
@@ -89,7 +108,14 @@ export function Editor({ element, initStyle, initContent }) {
   return (
     <div className='main'>
       <div className='preview' id='preview'>
-        <Element style={style} handleEditorView={handleEditorView} content={content} />
+        <Element
+          style={style}
+          handleEditorView={handleEditorView}
+          content={content}
+          setStyle={setStyle}
+          styleStorage={styleStorage}
+          setStyleStorage={setStyleStorage}
+        />
       </div>
       <div className='custom'>
         <div className='primary-div' style={{ width: `${width}%` }}>
@@ -98,7 +124,8 @@ export function Editor({ element, initStyle, initContent }) {
         <div onDrag={dragHandler} className='divider'></div>
         <div className='secondary-div' style={{ width: `${100 - width}%` }}>
           <div className='editor-section'>
-            <p>{editorView}</p>
+            <button onClick={() => setIsHover(false)}>normal</button>
+            <button onClick={() => setIsHover(true)}>hover</button>
           </div>
           <div className='editor-input'>
             <IconContext.Provider value={{ size: '20px' }}>
@@ -110,7 +137,12 @@ export function Editor({ element, initStyle, initContent }) {
                       <p>{data.section}</p>
                     </div>
                     <div>
-                      <input type='checkbox' id={data.section} onClick={handleShowProperty} />
+                      <input
+                        type='checkbox'
+                        id={data.section}
+                        defaultChecked={propertyShow[data.section.toLowerCase()]}
+                        onClick={handleShowProperty}
+                      />
                     </div>
                     <div className='input-section'>
                       {data.input.map((input) => {
@@ -134,6 +166,7 @@ export function Editor({ element, initStyle, initContent }) {
                                 name={input.property}
                                 onChange={input.type === 'text' ? handleChangeContent : handleChangeStyle}
                                 autoComplete='off'
+                                disabled={!propertyShow[data.section.toLowerCase()]}
                               />
                             )}
                             {input.type === 'select' && (
