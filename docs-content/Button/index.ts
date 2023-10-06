1dec4289848702939dde85bbc37f56f115c2a4fc
MuhammadRadifa <m.difa123456@gmail.com>
2023-10-06 18:57:38 +0800

    🔥chore : add initial value button

--- docs-content/Button/index.ts
+++ docs-content/Button/index.ts
@@ -1,5 +1,6 @@
 import { ButtonBasic } from './blocks/ButtonBasic';
 import { IEditor } from '@site/src/utils/interfaces/style';
+import { ButtonOutline } from './blocks/ButtonOutline';
 
 const BUTTON: Array<IEditor> = [
   {
@@ -12,11 +13,56 @@ const BUTTON: Array<IEditor> = [
         fontWeight: 'lighter',
         color: '#fff',
         padding: '10px',
+        boxShadow: '0px 0px 0px 0px rgba(0,0,0,0.20)',
+      },
+    },
+    initHover: {
+      '.button-container': {
+        backgroundColor: 'red',
+        cursor: 'pointer',
+        borderRadius: '0px',
+        fontWeight: 'lighter',
+        color: '#fff',
+        padding: '10px',
         borderColor: 'black',
         borderStyle: 'solid',
         borderWidth: '1px',
-        boxShadow: '0px 0px 0px 0px rgba(0,0,0,0.20)',
       },
+    },
+    initContent: {
+      '.button-container': 'Button ini Container',
+    },
+  },
+  {
+    element: ButtonOutline,
+    initStyle: {
+      '.button-outline': {
+        backgroundColor: '#4ba3e3',
+        cursor: 'pointer',
+        borderRadius: '0px',
+        fontWeight: 'lighter',
+        color: '#fff',
+        padding: '10px',
+        borderColor: 'black',
+        borderStyle: 'solid',
+        borderWidth: '1px',
+      },
+    },
+    initHover: {
+      '.button-outline': {
+        backgroundColor: 'red',
+        cursor: 'pointer',
+        borderRadius: '0px',
+        fontWeight: 'lighter',
+        color: '#fff',
+        padding: '10px',
+        borderColor: 'black',
+        borderStyle: 'solid',
+        borderWidth: '1px',
+      },
+    },
+    initContent: {
+      '.button-outline': 'Button ini Outline',
     },
   },
 ];
