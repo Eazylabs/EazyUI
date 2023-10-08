db39948ef5961f8391218edf5d42a9c751705822
MathcaTi <tehmatcha28@gmail.com>
2023-10-08 14:54:06 +0800

    🔥chore : delete title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,9 +47,8 @@ function ElementWrapper({ title, Svg }: IElemWrap) {
 }
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout title={`Hello from ${siteConfig.title}`} description='Description will go into a meta tag in <head />'>
+    <Layout description='Description will go into a meta tag in <head />'>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
