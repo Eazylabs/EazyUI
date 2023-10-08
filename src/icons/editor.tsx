c8acad8476115b73ac90aef016d79b9fe66a5cf5
MathcaTi <tehmatcha28@gmail.com>
2023-10-08 14:53:07 +0800

    🔥chore : update color icons

--- src/icons/editor.tsx
+++ src/icons/editor.tsx
@@ -5,7 +5,7 @@ export function WidthIcon() {
     <svg width='20' height='21' viewBox='0 0 20 21' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <path
         d='M16.384 5.44H18.24L14.128 16.176H13.568L10.416 8.688L7.264 16.176H6.688L2.576 5.44H4.448L7.088 12.576L10.112 5.264H10.704L13.744 12.576L16.384 5.44Z'
-        fill='white'
+        fill='#2c2c2c'
         fill-opacity='0.701961'
       />
     </svg>
@@ -17,7 +17,7 @@ export function HeightIcon() {
     <svg width='20' height='21' viewBox='0 0 20 21' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <path
         d='M5.6405 16V5.44H7.3365V9.648H12.6485V5.44H14.3445V16H12.6485V11.168H7.3365V16H5.6405Z'
-        fill='white'
+        fill='#2c2c2c'
         fill-opacity='0.701961'
       />
     </svg>
@@ -29,7 +29,7 @@ export function XHorizontal() {
     <svg width='20' height='21' viewBox='0 0 20 21' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <path
         d='M14.344 16H12.408L9.848 12.064L7.304 16H5.352L8.92 10.608L5.576 5.44H7.528L9.864 9.168L12.168 5.44H14.104L10.824 10.608L14.344 16Z'
-        fill='white'
+        fill='#2c2c2c'
         fill-opacity='0.701961'
       />
     </svg>
@@ -41,7 +41,7 @@ export function YVertical() {
     <svg width='20' height='21' viewBox='0 0 20 21' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <path
         d='M14.4951 5.44L10.8471 11.744V16H9.15106V11.744L5.50306 5.44H7.39106L9.99906 10.032L12.6231 5.44H14.4951Z'
-        fill='white'
+        fill='#2c2c2c'
         fill-opacity='0.701961'
       />
     </svg>
@@ -53,7 +53,7 @@ export function LetterSpacing() {
     <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <path
         d='M9 13L15 13M7 17L11.2717 7.60225C11.5031 7.09323 11.6188 6.83872 11.7791 6.75976C11.9184 6.69115 12.0816 6.69115 12.2209 6.75976C12.3812 6.83872 12.4969 7.09323 12.7283 7.60225L17 17M21 3V21M3 3L3 21'
-        stroke='#ffffffb3'
+        stroke='#2c2c2c'
         stroke-width='2'
         stroke-linecap='round'
         stroke-linejoin='round'
