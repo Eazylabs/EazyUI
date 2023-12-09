5cdc2e208caff77eefa5a8a1c5df42cd2895afa4
matchati <tehmatcha28@gmail.com>
2023-12-10 00:04:31 +0800

    🔥chore : add content alert neu with desc

--- docs-content/Alert/index.ts
+++ docs-content/Alert/index.ts
@@ -3,6 +3,7 @@ import { ALERTDANGERDESC } from './source-code/alertDangerDesc';
 import { ALERTINFO } from './source-code/alertInfo';
 import { ALERTINFODESC } from './source-code/alertInfoDesc';
 import { ALERTNEU } from './source-code/alertNeu';
+import { ALERTNEUDESC } from './source-code/alertNeuDesc';
 import { ALERTSUCCESS } from './source-code/alertSuccess';
 import { ALERTSUCCESSDESC } from './source-code/alertSuccessDesc';
 import { ALERTWARNING } from './source-code/alertWarning';
@@ -18,4 +19,5 @@ export const COMPONENTS = [
   ALERTSUCCESSDESC,
   ALERTWARNINGDESC,
   ALERTDANGERDESC,
+  ALERTNEUDESC,
 ];
