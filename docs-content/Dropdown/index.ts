64ec171f16d4d918a3e89a21e3f99bc086f689d3
matchati <tehmatcha28@gmail.com>
2023-12-09 14:54:29 +0800

    🔥chore : add content dropdown

--- docs-content/Dropdown/index.ts
+++ docs-content/Dropdown/index.ts
@@ -1,4 +1,5 @@
 import { DROPDOWNBASIC } from './source-code/dropdownBasic';
+import { DROPDOWNICON } from './source-code/dropdownIcon';
 import { DROPDOWNSECTION } from './source-code/dropdownSection';
 
-export const COMPONENTS = [DROPDOWNBASIC, DROPDOWNSECTION];
+export const COMPONENTS = [DROPDOWNBASIC, DROPDOWNSECTION, DROPDOWNICON];
