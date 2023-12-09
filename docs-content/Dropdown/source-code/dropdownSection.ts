2efa1917bd3c3060fd0cb4a487092729a85ad258
matchati <tehmatcha28@gmail.com>
2023-12-09 14:44:35 +0800

    🔥chore : update docs code

--- docs-content/Dropdown/source-code/dropdownSection.ts
+++ docs-content/Dropdown/source-code/dropdownSection.ts
@@ -1,20 +1,20 @@
 import DropdownSection from '../blocks/DropdownSection';
 
 export const DROPDOWNSECTION = {
-  html: `<div className='dropdown'>
-    <button className='btn btn-basic'>Hover me!</button>
-    <div className='dropdown-panel'>
-        <div className='dropdown-section'>
-            <div className='dropdown-section-title'>Section 1</div>
-            <div className='dropdown-item'>item 1</div>
-            <div className='dropdown-item'>item 2</div>
-            <div className='dropdown-item'>item 3</div>
+  html: `<div class='dropdown'>
+    <button class='btn btn-basic'>Hover me!</button>
+    <div class='dropdown-panel'>
+        <div class='dropdown-section'>
+            <div class='dropdown-section-title'>Section 1</div>
+            <div class='dropdown-item'>item 1</div>
+            <div class='dropdown-item'>item 2</div>
+            <div class='dropdown-item'>item 3</div>
         </div>
-        <div className='dropdown-section'>
-            <div className='dropdown-section-title'>Section 2</div>
-            <div className='dropdown-item'>item 4</div>
-            <div className='dropdown-item'>item 5</div>
-            <div className='dropdown-item'>item 6</div>
+        <div class='dropdown-section'>
+            <div class='dropdown-section-title'>Section 2</div>
+            <div class='dropdown-item'>item 4</div>
+            <div class='dropdown-item'>item 5</div>
+            <div class='dropdown-item'>item 6</div>
         </div>
     </div>
 </div>`,
