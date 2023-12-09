bb4047383e1577862c450bebe40fa053007cb6d7
matchati <tehmatcha28@gmail.com>
2023-12-09 18:08:54 +0800

    🔥chore : add content doc modal image

--- docs-content/Modal/index.ts
+++ docs-content/Modal/index.ts
@@ -1,4 +1,5 @@
 import { MODALBASIC } from './source-code/modalBasic';
 import { MODALBASICNEU } from './source-code/modalBasicNeu';
+import { MODALIMAGE } from './source-code/modalImage';
 
-export const COMPONENTS = [MODALBASIC, MODALBASICNEU];
+export const COMPONENTS = [MODALBASIC, MODALBASICNEU, MODALIMAGE];
