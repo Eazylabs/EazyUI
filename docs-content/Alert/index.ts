03d65524d045ec724206b2b7f023d421ec80d7e2
matchati <tehmatcha28@gmail.com>
2023-12-09 22:20:56 +0800

    🔥chore : add content alert info with desc

--- docs-content/Alert/index.ts
+++ docs-content/Alert/index.ts
@@ -1,7 +1,8 @@
 import { ALERTDANGER } from './source-code/alertDanger';
 import { ALERTINFO } from './source-code/alertInfo';
+import { ALERTINFODESC } from './source-code/alertInfoDesc';
 import { ALERTNEU } from './source-code/alertNeu';
 import { ALERTSUCCESS } from './source-code/alertSuccess';
 import { ALERTWARNING } from './source-code/alertWarning';
 
-export const COMPONENTS = [ALERTINFO, ALERTSUCCESS, ALERTWARNING, ALERTDANGER, ALERTNEU];
+export const COMPONENTS = [ALERTINFO, ALERTSUCCESS, ALERTWARNING, ALERTDANGER, ALERTNEU, ALERTINFODESC];
