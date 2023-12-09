bdedf92bc1bfd4b62001bd21406d26c8abd9f4ce
matchati <tehmatcha28@gmail.com>
2023-12-09 22:30:09 +0800

    🔥chore : add content alert success with info

--- docs-content/Alert/index.ts
+++ docs-content/Alert/index.ts
@@ -3,6 +3,15 @@ import { ALERTINFO } from './source-code/alertInfo';
 import { ALERTINFODESC } from './source-code/alertInfoDesc';
 import { ALERTNEU } from './source-code/alertNeu';
 import { ALERTSUCCESS } from './source-code/alertSuccess';
+import { ALERTSUCCESSDESC } from './source-code/alertSuccessDesc';
 import { ALERTWARNING } from './source-code/alertWarning';
 
-export const COMPONENTS = [ALERTINFO, ALERTSUCCESS, ALERTWARNING, ALERTDANGER, ALERTNEU, ALERTINFODESC];
+export const COMPONENTS = [
+  ALERTINFO,
+  ALERTSUCCESS,
+  ALERTWARNING,
+  ALERTDANGER,
+  ALERTNEU,
+  ALERTINFODESC,
+  ALERTSUCCESSDESC,
+];
