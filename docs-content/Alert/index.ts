a5d5908bdc260ec88de92162498e6052030c7d06
matchati <tehmatcha28@gmail.com>
2023-12-09 21:46:39 +0800

    🔥chore : add content alert warning

--- docs-content/Alert/index.ts
+++ docs-content/Alert/index.ts
@@ -1,4 +1,5 @@
 import { ALERTINFO } from './source-code/alertInfo';
 import { ALERTSUCCESS } from './source-code/alertSuccess';
+import { ALERTWARNING } from './source-code/alertWarning';
 
-export const COMPONENTS = [ALERTINFO, ALERTSUCCESS];
+export const COMPONENTS = [ALERTINFO, ALERTSUCCESS, ALERTWARNING];
