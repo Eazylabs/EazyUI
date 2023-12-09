6b213055b487e1b3c370cd929373724154c29670
matchati <tehmatcha28@gmail.com>
2023-12-10 00:55:36 +0800

    🔥chore : add content progress warning

--- docs-content/Progress/index.ts
+++ docs-content/Progress/index.ts
@@ -1,4 +1,5 @@
 import { PROGRESSINFO } from './source-code/progressInfo';
 import { PROGRESSSUCCESS } from './source-code/progressSuccess';
+import { PROGRESSWARNING } from './source-code/progressWarning';
 
-export const COMPONENTS = [PROGRESSINFO, PROGRESSSUCCESS];
+export const COMPONENTS = [PROGRESSINFO, PROGRESSSUCCESS, PROGRESSWARNING];
