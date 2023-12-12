316ba12b859d15f143316a4ea6f4cfb5bcddde36
matchati <tehmatcha28@gmail.com>
2023-12-12 14:53:23 +0800

    🔥chore : add content badge warn outline

--- docs-content/Badge/index.ts
+++ docs-content/Badge/index.ts
@@ -5,6 +5,7 @@ import { BADGENEU } from './source-code/badgeNeu';
 import { BADGESUCCESS } from './source-code/badgeSuccess';
 import { BADGESUCCESSOUTLINE } from './source-code/badgeSuccessOutline';
 import { BADGEWARN } from './source-code/badgeWarn';
+import { BADGEWARNOUTLINE } from './source-code/badgeWarnOutline';
 
 export const COMPONENTS = [
   BADGEINFO,
@@ -14,4 +15,5 @@ export const COMPONENTS = [
   BADGENEU,
   BADGEINFOOUTLINE,
   BADGESUCCESSOUTLINE,
+  BADGEWARNOUTLINE,
 ];
