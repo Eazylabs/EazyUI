1bc22f715d47068f3c150dc9f7b273639a783945
matchati <tehmatcha28@gmail.com>
2023-12-12 14:36:34 +0800

    🔥chore : add content badge neu

--- docs-content/Badge/index.ts
+++ docs-content/Badge/index.ts
@@ -1,6 +1,7 @@
 import { BADGEDANGER } from './source-code/badgeDanger';
 import { BADGEINFO } from './source-code/badgeInfo';
+import { BADGENEU } from './source-code/badgeNeu';
 import { BADGESUCCESS } from './source-code/badgeSuccess';
 import { BADGEWARN } from './source-code/badgeWarn';
 
-export const COMPONENTS = [BADGEINFO, BADGESUCCESS, BADGEWARN, BADGEDANGER];
+export const COMPONENTS = [BADGEINFO, BADGESUCCESS, BADGEWARN, BADGEDANGER, BADGENEU];
---
+++ docs-content/Badge/source-code/badgeNeu.ts
@@ -0,0 +1,16 @@
+import BadgeNeu from '../blocks/BadgeNeu';
+
+export const BADGENEU = {
+  html: `<div class='badges badge-neu'>Infomation</div>`,
+  css: `.badges {
+  padding: 6px 16px;
+  border-radius: 99px;
+}
+
+.badge-neu {
+  color: #183153;
+  border: 1px solid #183153;
+  box-shadow: 2px 2px #183153;
+}`,
+  element: BadgeNeu,
+};
