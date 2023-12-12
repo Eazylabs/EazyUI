3ddb9f7af17ba3b8e8edfe66a96a0ba1c02c7c9a
matchati <tehmatcha28@gmail.com>
2023-12-12 14:15:21 +0800

    ❌delete: delete unused file

--- docs-content/Badge/blocks/BadgeBasic.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import '../styles/style.css';
-
-export default function BadgeBasic() {
-  return <div className='badges badge-basic'>Infomation</div>;
-}
--- docs-content/Badge/blocks/BadgeOutline.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import '../styles/style.css';
-
-export default function BadgeOutline() {
-  return <div className='badges badge-outline'>Infomation</div>;
-}
