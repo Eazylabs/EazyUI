9a7a049a33477baa470c361298bbdafcde1e1e32
matchati <tehmatcha28@gmail.com>
2023-12-12 14:01:07 +0800

    🚀feat : add docs badge

---
+++ docs-content/Badge/index.ts
@@ -0,0 +1,3 @@
+import { BADGEBASIC } from './source-code/badgeBasic';
+
+export const COMPONENTS = [BADGEBASIC];
---
+++ docs-content/Badge/source-code/badgeBasic.ts
@@ -0,0 +1,17 @@
+import BadgeBasic from '../blocks/BadgeBasic';
+
+export const BADGEBASIC = {
+  html: `<div class='badges badge-basic'>Infomation</div>`,
+  css: `.badges {
+  padding: 6px 16px;
+  border-radius: 99px;
+}
+
+.badge-basic {
+  color: #fff;
+  background-color: #4ba3e3;
+}
+
+  `,
+  element: BadgeBasic,
+};
