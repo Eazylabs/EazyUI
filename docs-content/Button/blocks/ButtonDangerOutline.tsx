87c819f5cbfb86ae4b778c057dc4ec9fd01cdd76
ifnuu01 <ifnuu01@gmail.com>
2023-12-15 17:49:43 +0800

    🔥chore : add content button danger outline

---
+++ docs-content/Button/blocks/ButtonDangerOutline.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import '../styles/style.css';
+
+export default function ButtonDangerOutline() {
+  return <div className='btn btn-danger-outline'>Button Danger Outline</div>;
+}
