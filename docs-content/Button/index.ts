2eafa525816bb00a64fccb4dcef31c826e84b5ec
ifnuu01 <ifnuu01@gmail.com>
2023-12-15 16:34:59 +0800

    🔥chore : add content button success

--- docs-content/Button/index.ts
+++ docs-content/Button/index.ts
@@ -4,5 +4,6 @@ import { BUTTONOUTLINE } from './source-code/buttonOutline';
 import { BUTTONNEUBASIC } from './source-code/buttonNeuBasic';
 import { BUTTONNEUOUTLINE } from './source-code/buttonNeuOutline';
 import { BUTTONNEUICON } from './source-code/buttonNeuIcon';
+import { BUTTONSUCCESS } from './source-code/buttonSuccess';
 
-export const COMPONENTS = [BUTTONBASIC, BUTTONOUTLINE, BUTTONICON, BUTTONNEUBASIC, BUTTONNEUOUTLINE, BUTTONNEUICON];
+export const COMPONENTS = [BUTTONBASIC, BUTTONOUTLINE, BUTTONICON, BUTTONNEUBASIC, BUTTONNEUOUTLINE, BUTTONNEUICON,BUTTONSUCCESS];
