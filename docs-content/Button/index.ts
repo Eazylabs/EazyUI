1547b2fb1bc2c42229f14d600f048ed9ec1c8d43
ifnuu01 <ifnuu01@gmail.com>
2023-12-15 17:09:52 +0800

    🔥chore : add content warning

--- docs-content/Button/index.ts
+++ docs-content/Button/index.ts
@@ -5,5 +5,6 @@ import { BUTTONNEUBASIC } from './source-code/buttonNeuBasic';
 import { BUTTONNEUOUTLINE } from './source-code/buttonNeuOutline';
 import { BUTTONNEUICON } from './source-code/buttonNeuIcon';
 import { BUTTONSUCCESS } from './source-code/buttonSuccess';
+import { BUTTONWARNING } from './source-code/buttonWarning';
 
-export const COMPONENTS = [BUTTONBASIC, BUTTONOUTLINE, BUTTONICON, BUTTONNEUBASIC, BUTTONNEUOUTLINE, BUTTONNEUICON,BUTTONSUCCESS];
+export const COMPONENTS = [BUTTONBASIC, BUTTONOUTLINE, BUTTONICON, BUTTONNEUBASIC, BUTTONNEUOUTLINE, BUTTONNEUICON,BUTTONSUCCESS,BUTTONWARNING];
