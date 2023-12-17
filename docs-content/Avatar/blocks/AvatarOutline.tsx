2d05f45b107b5f7218fc4f97b1092ff126e1635d
MatchaTi <tehmatcha28@gmail.com>
2023-12-17 14:32:04 +0800

    🔥chore : add img source

--- docs-content/Avatar/blocks/AvatarOutline.tsx
+++ docs-content/Avatar/blocks/AvatarOutline.tsx
@@ -4,9 +4,15 @@ import '../styles/style.css';
 export default function AvatarOutline() {
   return (
     <div className='avatar-wrapper'>
-      <div className='avatar avatar-outline avatar-circle'></div>
-      <div className='avatar avatar-outline avatar-rounded'></div>
-      <div className='avatar avatar-outline avatar-square'></div>
+      <div className='avatar avatar-outline avatar-circle'>
+        <img src='https://i.pravatar.cc/80' alt='avatar' />
+      </div>
+      <div className='avatar avatar-outline avatar-rounded'>
+        <img src='https://i.pravatar.cc/80' alt='avatar' />
+      </div>
+      <div className='avatar avatar-outline avatar-square'>
+        <img src='https://i.pravatar.cc/80' alt='avatar' />
+      </div>
     </div>
   );
 }
