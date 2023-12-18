e1feab3a6754ab454e699c3327a705dc4c2634d0
MatchaTi <tehmatcha28@gmail.com>
2023-12-18 12:09:34 +0800

    🔥chore : add content checkbox warning

--- docs-content/Checkbox/index.ts
+++ docs-content/Checkbox/index.ts
@@ -1,4 +1,5 @@
 import { CHECKBOXINFO } from './source-code/checkBoxInfo';
 import { CHECKBOXSUCCESS } from './source-code/checkBoxSuccess';
+import { CHECKBOXWARNING } from './source-code/checkBoxWarning';
 
-export const COMPONENTS = [CHECKBOXINFO, CHECKBOXSUCCESS];
+export const COMPONENTS = [CHECKBOXINFO, CHECKBOXSUCCESS, CHECKBOXWARNING];
