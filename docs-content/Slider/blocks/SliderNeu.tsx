ac078a14cb61ac2025eef819c3eff9b087a460cd
MatchaTi <tehmatcha28@gmail.com>
2023-12-18 16:02:37 +0800

    🔥chore : add content slider neu

---
+++ docs-content/Slider/blocks/SliderNeu.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../styles/style.css';
+
+export default function SliderNeu() {
+  return <input type='range' className='slider slider-neu' min='0' max='100' />;
+}
--- docs-content/Slider/index.ts
+++ docs-content/Slider/index.ts
@@ -1,6 +1,7 @@
 import { SLIDERDANGER } from './source-code/sliderDanger';
 import { SLIDERINFO } from './source-code/sliderInfo';
+import { SLIDERNEU } from './source-code/sliderNeu';
 import { SLIDERSUCCESS } from './source-code/sliderSuccess';
 import { SLIDERWARNING } from './source-code/sliderWarning';
 
-export const COMPONENTS = [SLIDERINFO, SLIDERSUCCESS, SLIDERWARNING, SLIDERDANGER];
+export const COMPONENTS = [SLIDERINFO, SLIDERSUCCESS, SLIDERWARNING, SLIDERDANGER, SLIDERNEU];
