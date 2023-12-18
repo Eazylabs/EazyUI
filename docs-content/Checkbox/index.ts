db0c9233d99cdbe2703c66ddd44eff9718ff9350
MatchaTi <tehmatcha28@gmail.com>
2023-12-18 12:15:04 +0800

    🔥chore : add content checkbox danger

--- docs-content/Checkbox/index.ts
+++ docs-content/Checkbox/index.ts
@@ -1,5 +1,6 @@
+import { CHECKBOXDANGER } from './source-code/checkBoxDanger';
 import { CHECKBOXINFO } from './source-code/checkBoxInfo';
 import { CHECKBOXSUCCESS } from './source-code/checkBoxSuccess';
 import { CHECKBOXWARNING } from './source-code/checkBoxWarning';
 
-export const COMPONENTS = [CHECKBOXINFO, CHECKBOXSUCCESS, CHECKBOXWARNING];
+export const COMPONENTS = [CHECKBOXINFO, CHECKBOXSUCCESS, CHECKBOXWARNING, CHECKBOXDANGER];
