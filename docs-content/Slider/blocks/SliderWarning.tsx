2f8f9fd14b314a00b5f245ae53e748e78859987e
MatchaTi <tehmatcha28@gmail.com>
2023-12-18 15:57:46 +0800

    🔥chore : add content slider warning

---
+++ docs-content/Slider/blocks/SliderWarning.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../styles/style.css';
+
+export default function SliderWarning() {
+  return <input type='range' className='slider slider-warning' min='0' max='100' />;
+}
--- docs-content/Slider/index.ts
+++ docs-content/Slider/index.ts
@@ -1,4 +1,5 @@
 import { SLIDERINFO } from './source-code/sliderInfo';
 import { SLIDERSUCCESS } from './source-code/sliderSuccess';
+import { SLIDERWARNING } from './source-code/sliderWarning';
 
-export const COMPONENTS = [SLIDERINFO, SLIDERSUCCESS];
+export const COMPONENTS = [SLIDERINFO, SLIDERSUCCESS, SLIDERWARNING];
