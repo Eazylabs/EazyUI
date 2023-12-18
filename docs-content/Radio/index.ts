ec2bbec8e6a2e2916f48b01285a0b7810ec12e81
MatchaTi <tehmatcha28@gmail.com>
2023-12-18 14:27:44 +0800

    🔥chore : add content radio neu

--- docs-content/Radio/index.ts
+++ docs-content/Radio/index.ts
@@ -1,6 +1,7 @@
 import { RADIODANGER } from './source-code/radioDanger';
 import { RADIOINFO } from './source-code/radioInfo';
+import { RADIONEU } from './source-code/radioNeu';
 import { RADIOSUCCESS } from './source-code/radioSuccess';
 import { RADIOWARNING } from './source-code/radioWarning';
 
-export const COMPONENTS = [RADIOINFO, RADIOSUCCESS, RADIOWARNING, RADIODANGER];
+export const COMPONENTS = [RADIOINFO, RADIOSUCCESS, RADIOWARNING, RADIODANGER, RADIONEU];
