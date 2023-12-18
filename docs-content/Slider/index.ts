e9db48a7bd31b4de55273a501f9b232d0d7b7665
MatchaTi <tehmatcha28@gmail.com>
2023-12-18 15:59:29 +0800

    🔥chore : add content slider danger

--- docs-content/Slider/index.ts
+++ docs-content/Slider/index.ts
@@ -1,5 +1,6 @@
+import { SLIDERDANGER } from './source-code/sliderDanger';
 import { SLIDERINFO } from './source-code/sliderInfo';
 import { SLIDERSUCCESS } from './source-code/sliderSuccess';
 import { SLIDERWARNING } from './source-code/sliderWarning';
 
-export const COMPONENTS = [SLIDERINFO, SLIDERSUCCESS, SLIDERWARNING];
+export const COMPONENTS = [SLIDERINFO, SLIDERSUCCESS, SLIDERWARNING, SLIDERDANGER];
