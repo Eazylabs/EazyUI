6169ca356e667a36fc59fa0e926d1f10b80b467c
MatchaTi <tehmatcha28@gmail.com>
2023-12-18 20:08:18 +0800

    🔥chore : add content select warning

--- docs-content/Select/index.ts
+++ docs-content/Select/index.ts
@@ -2,5 +2,6 @@ import { SELECTBORDER } from './source-code/selectBorder';
 import { SELECTINFO } from './source-code/selectInfo';
 import { SELECTNOBORDER } from './source-code/selectNoBorder';
 import { SELECTSUCCESS } from './source-code/selectSuccess';
+import { SELECTWARNING } from './source-code/selectWarning';
 
-export const COMPONENTS = [SELECTBORDER, SELECTNOBORDER, SELECTINFO, SELECTSUCCESS];
+export const COMPONENTS = [SELECTBORDER, SELECTNOBORDER, SELECTINFO, SELECTSUCCESS, SELECTWARNING];
