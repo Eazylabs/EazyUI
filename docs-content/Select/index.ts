284649f219d5b59b412aceee6b31d01ecbef36b9
MatchaTi <tehmatcha28@gmail.com>
2023-12-18 20:04:24 +0800

    🔥chore : add content select info

--- docs-content/Select/index.ts
+++ docs-content/Select/index.ts
@@ -1,4 +1,5 @@
 import { SELECTBORDER } from './source-code/selectBorder';
+import { SELECTINFO } from './source-code/selectInfo';
 import { SELECTNOBORDER } from './source-code/selectNoBorder';
 
-export const COMPONENTS = [SELECTBORDER, SELECTNOBORDER];
+export const COMPONENTS = [SELECTBORDER, SELECTNOBORDER, SELECTINFO];
