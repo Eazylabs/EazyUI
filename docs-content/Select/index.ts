44da65b17e305c15fb03efdb471a4038a419927e
MatchaTi <tehmatcha28@gmail.com>
2023-12-18 20:06:00 +0800

    🔥chore : add content select success

--- docs-content/Select/index.ts
+++ docs-content/Select/index.ts
@@ -1,5 +1,6 @@
 import { SELECTBORDER } from './source-code/selectBorder';
 import { SELECTINFO } from './source-code/selectInfo';
 import { SELECTNOBORDER } from './source-code/selectNoBorder';
+import { SELECTSUCCESS } from './source-code/selectSuccess';
 
-export const COMPONENTS = [SELECTBORDER, SELECTNOBORDER, SELECTINFO];
+export const COMPONENTS = [SELECTBORDER, SELECTNOBORDER, SELECTINFO, SELECTSUCCESS];
