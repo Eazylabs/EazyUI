7c77f64f13aa57411fb0a6d2fc2f0b0593912fa2
MatchaTi <tehmatcha28@gmail.com>
2023-12-18 12:02:13 +0800

    🔥chore : update docs

--- docs-content/Checkbox/source-code/checkBoxInfo.ts
+++ docs-content/Checkbox/source-code/checkBoxInfo.ts
@@ -1,7 +1,42 @@
 import CheckboxInfo from '../blocks/CheckboxInfo';
 
 export const CHECKBOXINFO = {
-  html: ``,
-  css: ``,
+  html: `<input type='checkbox' class='checkbox checkbox-info' />`,
+  css: `.checkbox {
+  appearance: none;
+  font: inherit;
+  color: inherit;
+  width: 32px;
+  height: 32px;
+  border-radius: 8px;
+  transform: translateY(-0.075em);
+  display: grid;
+  place-content: center;
+  cursor: pointer;
+}
+
+.checkbox-info {
+  border: 1.5px solid #4ba3e3;
+}
+  
+.checkbox::before {
+  content: '';
+  width: 16px;
+  height: 16px;
+  clip-path: polygon(14% 44%, 0 65%, 50% 100%, 100% 16%, 80% 0%, 43% 62%);
+  transform: scale(0);
+  transform-origin: bottom;
+  transition: 120ms transform ease-in-out;
+  background-color: #fff;
+}
+
+.checkbox:checked::before {
+  transform: scale(1);
+}
+
+.checkbox-info:checked {
+  background-color: #4ba3e3;
+  color: #fff;
+}`,
   element: CheckboxInfo,
 };
