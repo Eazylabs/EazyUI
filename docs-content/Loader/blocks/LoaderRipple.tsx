e7e9fd444230f1c142b0c8de943df799725e43ac
MatchaTi <tehmatcha28@gmail.com>
2023-12-19 17:50:52 +0800

    🔥chore : add content loader ripple

---
+++ docs-content/Loader/blocks/LoaderRipple.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/style.css';
+
+export default function LoaderRipple() {
+  return (
+    <div className='loader loader-ripple'>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
--- docs-content/Loader/index.ts
+++ docs-content/Loader/index.ts
@@ -1,6 +1,7 @@
 import { LOADERDUALRING } from './source-code/loaderDualRing';
 import { LOADERELLIPSIS } from './source-code/loaderEllipsis';
 import { LOADERRING } from './source-code/loaderRing';
+import { LOADERRIPPLE } from './source-code/loaderRipple';
 import { LOADERROLLER } from './source-code/loaderRoller';
 
-export const COMPONENTS = [LOADERRING, LOADERDUALRING, LOADERROLLER, LOADERELLIPSIS];
+export const COMPONENTS = [LOADERRING, LOADERDUALRING, LOADERROLLER, LOADERELLIPSIS, LOADERRIPPLE];
