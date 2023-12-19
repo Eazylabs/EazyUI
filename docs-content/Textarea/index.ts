38ea3d4a1ee69fb0ccbc63997a27e2a1e5c7291e
MatchaTi <tehmatcha28@gmail.com>
2023-12-19 11:45:48 +0800

    🔥chore : add content textarea danger

--- docs-content/Textarea/index.ts
+++ docs-content/Textarea/index.ts
@@ -1,5 +1,6 @@
+import { TEXTAREADANGER } from './source-code/textareaDanger';
 import { TEXTAREAINFO } from './source-code/textareaInfo';
 import { TEXTAREASUCCESS } from './source-code/textareaSuccess';
 import { TEXTAREAWARNING } from './source-code/textareaWarning';
 
-export const COMPONENTS = [TEXTAREAINFO, TEXTAREASUCCESS, TEXTAREAWARNING];
+export const COMPONENTS = [TEXTAREAINFO, TEXTAREASUCCESS, TEXTAREAWARNING, TEXTAREADANGER];
