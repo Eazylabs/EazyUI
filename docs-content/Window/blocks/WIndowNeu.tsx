511d6a5fe44dbce458d422e181f8c8f0a668ebec
ifnuu01 <ifnuu01@gmail.com>
2023-12-19 09:49:23 +0800

    🔥chore : add content window neu

---
+++ docs-content/Window/blocks/WIndowNeu.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/style.css';
+
+export default function WindowNeu() {
+  return (
+    <div className="mockup neu">
+        <div className="window-mockup neu"></div>
+    </div>
+  );
+}
