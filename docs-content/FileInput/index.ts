2f9c231a8245a69a3979368d5f59153607a7be5d
MatchaTi <tehmatcha28@gmail.com>
2023-12-19 14:40:17 +0800

    🔥chore : add content file input warning

--- docs-content/FileInput/index.ts
+++ docs-content/FileInput/index.ts
@@ -1,4 +1,5 @@
 import { FILEINFO } from './source-code/fileInfo';
 import { FILESUCCESS } from './source-code/fileSuccess';
+import { FILEWARNING } from './source-code/fileWarning';
 
-export const COMPONENTS = [FILEINFO, FILESUCCESS];
+export const COMPONENTS = [FILEINFO, FILESUCCESS, FILEWARNING];
---
+++ docs-content/FileInput/source-code/fileWarning.ts
@@ -0,0 +1,30 @@
+import FileWarning from '../blocks/FileWarning';
+
+export const FILEWARNING = {
+  html: `<input type="file" class="file-input file-warning" />`,
+  css: `.file-input {
+  display: block;
+  width: clamp(400px, 75%, 600px);
+  border: 4px solid #4ba3e3;
+  border-radius: 8px;
+  cursor: pointer;
+}
+
+.file-input::file-selector-button {
+  background-color: #4ba3e3;
+  padding: 16px 32px;
+  border-radius: 8px;
+  border: none;
+  color: #fff;
+  text-transform: uppercase;
+}
+
+.file-warning {
+  border-color: #ffd43b;
+}
+
+.file-warning::file-selector-button {
+  background-color: #ffd43b;
+}`,
+  element: FileWarning,
+};
