c8974326bfc7c0bca5e86d5f92edd2ebf004744f
MatchaTi <tehmatcha28@gmail.com>
2023-12-19 17:54:05 +0800

    🔥chore : add content loader spinner

---
+++ docs-content/Loader/blocks/LoaderSpinner.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/style.css';
+
+export default function LoaderSpinner() {
+  return (
+    <div className='loader loader-spinner'>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
--- docs-content/Loader/index.ts
+++ docs-content/Loader/index.ts
@@ -3,5 +3,6 @@ import { LOADERELLIPSIS } from './source-code/loaderEllipsis';
 import { LOADERRING } from './source-code/loaderRing';
 import { LOADERRIPPLE } from './source-code/loaderRipple';
 import { LOADERROLLER } from './source-code/loaderRoller';
+import { LOADERSPINNER } from './source-code/loaderSpinner';
 
-export const COMPONENTS = [LOADERRING, LOADERDUALRING, LOADERROLLER, LOADERELLIPSIS, LOADERRIPPLE];
+export const COMPONENTS = [LOADERRING, LOADERDUALRING, LOADERROLLER, LOADERELLIPSIS, LOADERRIPPLE, LOADERSPINNER];
