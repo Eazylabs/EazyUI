c502443f1d7cb805b70a018e8521fa313001e7ac
MatchaTi <tehmatcha28@gmail.com>
2023-12-19 13:41:27 +0800

    🔥chore : add content switch success

--- docs-content/Switch/index.ts
+++ docs-content/Switch/index.ts
@@ -1,3 +1,4 @@
 import { SWITCHINFO } from './source-code/switchInfo';
+import { SWITCHSUCCESS } from './source-code/switchSuccess';
 
-export const COMPONENTS = [SWITCHINFO];
+export const COMPONENTS = [SWITCHINFO, SWITCHSUCCESS];
