1311802ed0e6cb53aebf897267263b6b4eed8da5
MatchaTi <tehmatcha28@gmail.com>
2023-12-19 11:44:25 +0800

    🔥chore : add content textarea warning

--- docs-content/Textarea/index.ts
+++ docs-content/Textarea/index.ts
@@ -1,4 +1,5 @@
 import { TEXTAREAINFO } from './source-code/textareaInfo';
 import { TEXTAREASUCCESS } from './source-code/textareaSuccess';
+import { TEXTAREAWARNING } from './source-code/textareaWarning';
 
-export const COMPONENTS = [TEXTAREAINFO, TEXTAREASUCCESS];
+export const COMPONENTS = [TEXTAREAINFO, TEXTAREASUCCESS, TEXTAREAWARNING];
