0dfcc67c72732fda14d0378af9ba0d81b3f671f5
MatchaTi <tehmatcha28@gmail.com>
2023-12-19 14:42:17 +0800

    🔥chore : add content file input danger

--- docs-content/FileInput/index.ts
+++ docs-content/FileInput/index.ts
@@ -1,5 +1,6 @@
+import { FILEDANGER } from './source-code/fileDanger';
 import { FILEINFO } from './source-code/fileInfo';
 import { FILESUCCESS } from './source-code/fileSuccess';
 import { FILEWARNING } from './source-code/fileWarning';
 
-export const COMPONENTS = [FILEINFO, FILESUCCESS, FILEWARNING];
+export const COMPONENTS = [FILEINFO, FILESUCCESS, FILEWARNING, FILEDANGER];
---
+++ docs-content/FileInput/source-code/fileDanger.ts
@@ -0,0 +1,30 @@
+import FileDanger from '../blocks/FileDanger';
+
+export const FILEDANGER = {
+  html: `<input type="file" class="file-input file-danger" />`,
+  css: `.file-input {
+  display: block;
+  width: clamp(400px, 75%, 600px);
+  border: 4px solid #4ba3e3;
+  border-radius: 8px;
+  cursor: pointer;
+}
+
+.file-input::file-selector-button {
+  background-color: #4ba3e3;
+  padding: 16px 32px;
+  border-radius: 8px;
+  border: none;
+  color: #fff;
+  text-transform: uppercase;
+}
+
+.file-danger {
+  border-color: #e03131;
+}
+
+.file-danger::file-selector-button {
+  background-color: #e03131;
+}`,
+  element: FileDanger,
+};
