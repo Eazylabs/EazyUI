29e89fc004f01c34b04d4d025122b52145341b05
MatchaTi <tehmatcha28@gmail.com>
2023-12-19 15:49:13 +0800

    🔥chore : add content accordion neu

---
+++ docs-content/Accordion/blocks/AccordionNeu.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../styles/style.css';
+
+export default function AccordionNeu() {
+  return (
+    <div>
+      <details className='accordion accordion-neu'>
+        <summary className='accordion-title'>
+          What's an accordion?
+          <div className='chevron-icon'></div>
+        </summary>
+        <div className='content-wrapper'>
+          <p className='accordion-desc'>
+            An accordion is a list of headers that hide or reveal additional content when selected/opened. You may see
+            them often on FAQ pages, where it makes it easy for users to scan the list of questions without getting
+            distracted by answers and a lot of text, and giving them control by clicking on the questions that pertain
+            to them.
+          </p>
+        </div>
+      </details>
+      <details className='accordion accordion-neu'>
+        <summary className='accordion-title'>
+          Siapa member Eazy Labs yang paling ganteng?
+          <div className='chevron-icon'></div>
+        </summary>
+        <div className='content-wrapper'>
+          <p className='accordion-desc'>Yaa jelas Adi!</p>
+        </div>
+      </details>
+    </div>
+  );
+}
--- docs-content/Accordion/index.ts
+++ docs-content/Accordion/index.ts
@@ -1,6 +1,7 @@
 import { ACCORDIONDANGER } from './source-code/accordionDanger';
 import { ACCORDIONINFO } from './source-code/accordionInfo';
+import { ACCORDIONNEU } from './source-code/accordionNeu';
 import { ACCORDIONSUCCESS } from './source-code/accordionSuccess';
 import { ACCORDIONWARNING } from './source-code/accordionWarning';
 
-export const COMPONENTS = [ACCORDIONINFO, ACCORDIONSUCCESS, ACCORDIONWARNING, ACCORDIONDANGER];
+export const COMPONENTS = [ACCORDIONINFO, ACCORDIONSUCCESS, ACCORDIONWARNING, ACCORDIONDANGER, ACCORDIONNEU];
