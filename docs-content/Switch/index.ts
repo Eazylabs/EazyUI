1d2439bb2938bc3cdc89ebfad17bc513444c606f
MatchaTi <tehmatcha28@gmail.com>
2023-12-19 13:43:14 +0800

    🔥chore : add content switch warning

--- docs-content/Switch/index.ts
+++ docs-content/Switch/index.ts
@@ -1,4 +1,5 @@
 import { SWITCHINFO } from './source-code/switchInfo';
 import { SWITCHSUCCESS } from './source-code/switchSuccess';
+import { SWITCHWARNING } from './source-code/switchWarning';
 
-export const COMPONENTS = [SWITCHINFO, SWITCHSUCCESS];
+export const COMPONENTS = [SWITCHINFO, SWITCHSUCCESS, SWITCHWARNING];
