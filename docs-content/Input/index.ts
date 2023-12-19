41cc3fd71f7672b19913d4e509f6a56e10b95d56
MatchaTi <tehmatcha28@gmail.com>
2023-12-19 11:01:25 +0800

    🔥chore : add content input danger

--- docs-content/Input/index.ts
+++ docs-content/Input/index.ts
@@ -1,5 +1,6 @@
+import { INPUTDANGER } from './source-code/inputDanger';
 import { INPUTINFO } from './source-code/inputInfo';
 import { INPUTSUCCESS } from './source-code/inputSuccess';
 import { INPUTWARNING } from './source-code/inputWarning';
 
-export const COMPONENTS = [INPUTINFO, INPUTSUCCESS, INPUTWARNING];
+export const COMPONENTS = [INPUTINFO, INPUTSUCCESS, INPUTWARNING, INPUTDANGER];
