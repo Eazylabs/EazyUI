9727cd9266fb216fa94b2e0ab1c67a371d280ee8
MatchaTi <tehmatcha28@gmail.com>
2023-12-19 15:44:32 +0800

    🔥chore : add content accordion success

--- docs-content/Accordion/index.ts
+++ docs-content/Accordion/index.ts
@@ -1,3 +1,4 @@
 import { ACCORDIONINFO } from './source-code/accordionInfo';
+import { ACCORDIONSUCCESS } from './source-code/accordionSuccess';
 
-export const COMPONENTS = [ACCORDIONINFO];
+export const COMPONENTS = [ACCORDIONINFO, ACCORDIONSUCCESS];
