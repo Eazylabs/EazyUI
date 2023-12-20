f6fe1c7baa7f97cde6a4247a1376a8a19e12c23d
MatchaTi <tehmatcha28@gmail.com>
2023-12-20 15:34:28 +0800

    🔥chore : add content menu icon label

--- docs-content/Menu/index.ts
+++ docs-content/Menu/index.ts
@@ -2,5 +2,6 @@ import { MENUBASIC } from './source-code/menuBasic';
 import { MENUHORIZONTAL } from './source-code/menuHorizontal';
 import { MENUICON } from './source-code/menuIcon';
 import { MENUICONHORIZONTAL } from './source-code/menuIconHorizontal';
+import { MENUICONLABEL } from './source-code/menuIconLabel';
 
-export const COMPONENTS = [MENUBASIC, MENUHORIZONTAL, MENUICON, MENUICONHORIZONTAL];
+export const COMPONENTS = [MENUBASIC, MENUHORIZONTAL, MENUICON, MENUICONHORIZONTAL, MENUICONLABEL];
