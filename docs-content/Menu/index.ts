086a7a4d059009fbeb7545e9b35006cc81c29827
MatchaTi <tehmatcha28@gmail.com>
2023-12-20 15:17:08 +0800

    🔥chore : add content menu icon

--- docs-content/Menu/index.ts
+++ docs-content/Menu/index.ts
@@ -1,4 +1,5 @@
 import { MENUBASIC } from './source-code/menuBasic';
 import { MENUHORIZONTAL } from './source-code/menuHorizontal';
+import { MENUICON } from './source-code/menuIcon';
 
-export const COMPONENTS = [MENUBASIC, MENUHORIZONTAL];
+export const COMPONENTS = [MENUBASIC, MENUHORIZONTAL, MENUICON];
---
+++ docs-content/Menu/source-code/menuIcon.ts
@@ -0,0 +1,86 @@
+import MenuIcon from '../blocks/MenuIcon';
+
+export const MENUICON = {
+  html: `<ul class="menu menu-icon">
+  <li class="menu-item">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      data-slot="icon"
+      class="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+      />
+    </svg>
+  </li>
+  <li class="menu-item">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      data-slot="icon"
+      class="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="m10.5 21 5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 0 1 6-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 0 1-3.827-5.802"
+      />
+    </svg>
+  </li>
+  <li class="menu-item">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      data-slot="icon"
+      class="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+      />
+    </svg>
+  </li>
+</ul>`,
+  css: `.menu {
+  padding: 8px;
+  border-radius: 8px;
+  background-color: #fff;
+  box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.1);
+  width: min(100%, 224px);
+}
+
+.menu-item {
+  padding: 8px 12px;
+  border-radius: 6px;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.menu-item:hover {
+  background-color: #f5f5f5;
+}
+
+svg {
+  width: 24px;
+  aspect-ratio: 1/1;
+}
+
+.menu-icon {
+  width: fit-content;
+}`,
+  element: MenuIcon,
+};
