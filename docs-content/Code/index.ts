bfcef6d1d71e9038c3e9f2f50dfcbde132dcbbc1
MatchaTi <tehmatcha28@gmail.com>
2023-12-20 10:49:56 +0800

    🔥chore : add content code multi line

--- docs-content/Code/index.ts
+++ docs-content/Code/index.ts
@@ -1,3 +1,4 @@
 import { CODEBASIC } from './source-code/codeBasic';
+import { CODEMULTILINE } from './source-code/codeMultiLine';
 
-export const COMPONENTS = [CODEBASIC];
+export const COMPONENTS = [CODEBASIC, CODEMULTILINE];
--- docs-content/Code/source-code/codeMultiLine.ts
+++ docs-content/Code/source-code/codeMultiLine.ts
@@ -1,13 +1,16 @@
-import CodeBasic from '../blocks/CodeBasic';
+import CodeMultiLine from '../blocks/CodeMultiLine';
 
-export const CODEBASIC = {
+export const CODEMULTILINE = {
   html: `<div class="mockup-code">
   <div class="three-dots">
     <div class="dot"></div>
     <div class="dot"></div>
     <div class="dot"></div>
   </div>
-  <pre><code>~ npm run dev</code></pre>
+  <pre class='text-info'><code>~ npm run dev</code></pre>
+  <pre class='text-success'><code>~ npm run dev</code></pre>
+  <pre class='text-warning'><code>~ npm run dev</code></pre>
+  <pre class='text-danger'><code>~ npm run dev</code></pre>
 </div>`,
   css: `.mockup-code {
   background-color: #183153;
@@ -32,6 +35,22 @@ export const CODEBASIC = {
 
 pre {
   padding: 8px 16px;
+}
+
+.text-info {
+  color: #4ba3e3;
+}
+
+.text-success {
+  color: #14b789;
+}
+
+.text-warning {
+  color: #ffd43b;
+}
+
+.text-danger {
+  color: #e03131;
 }`,
-  element: CodeBasic,
+  element: CodeMultiLine,
 };
