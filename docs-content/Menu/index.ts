64003d099e0d6fd74ba6297915dde9b981cc8d43
MatchaTi <tehmatcha28@gmail.com>
2023-12-20 15:25:02 +0800

    🔥chore : add content menu icon horizontal

--- docs-content/Menu/index.ts
+++ docs-content/Menu/index.ts
@@ -1,5 +1,6 @@
 import { MENUBASIC } from './source-code/menuBasic';
 import { MENUHORIZONTAL } from './source-code/menuHorizontal';
 import { MENUICON } from './source-code/menuIcon';
+import { MENUICONHORIZONTAL } from './source-code/menuIconHorizontal';
 
-export const COMPONENTS = [MENUBASIC, MENUHORIZONTAL, MENUICON];
+export const COMPONENTS = [MENUBASIC, MENUHORIZONTAL, MENUICON, MENUICONHORIZONTAL];
