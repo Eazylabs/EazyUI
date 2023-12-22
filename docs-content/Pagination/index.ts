7fe282acd34c9282c33178631bd3e9c666c4cd46
MatchaTi <tehmatcha28@gmail.com>
2023-12-22 18:22:18 +0800

    🔥chore : add content pagination range

--- docs-content/Pagination/index.ts
+++ docs-content/Pagination/index.ts
@@ -2,5 +2,6 @@ import { PAGINATIONBASIC } from "./source-code/paginationBasic";
 import { PAGINATIONNEU } from "./source-code/paginationNeu";
 import { PAGINATIONPREVIOUSNEXT } from "./source-code/paginationPreviousNext";
 import { PAGINATIONPREVIOUSNEXTNEU } from "./source-code/paginationPreviousNextNeu";
+import { PAGINATIONRANGE } from "./source-code/paginationRange";
 
-export const COMPONENTS = [PAGINATIONBASIC,PAGINATIONNEU,PAGINATIONPREVIOUSNEXT,PAGINATIONPREVIOUSNEXTNEU]
+export const COMPONENTS = [PAGINATIONBASIC,PAGINATIONNEU,PAGINATIONPREVIOUSNEXT,PAGINATIONPREVIOUSNEXTNEU,PAGINATIONRANGE]
