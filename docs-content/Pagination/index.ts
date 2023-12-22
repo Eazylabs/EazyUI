9c5b17ed061844262dfe75c4b3855d75b1b13820
MatchaTi <tehmatcha28@gmail.com>
2023-12-22 17:54:40 +0800

    🔥chore : add content pagination neu

--- docs-content/Pagination/index.ts
+++ docs-content/Pagination/index.ts
@@ -1,3 +1,4 @@
 import { PAGINATIONBASIC } from "./source-code/paginationBasic";
+import { PAGINATIONNEU } from "./source-code/paginationNeu";
 
-export const COMPONENTS = [PAGINATIONBASIC]
+export const COMPONENTS = [PAGINATIONBASIC,PAGINATIONNEU]
