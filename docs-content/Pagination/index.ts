5a51bb556ebcf5b2dae607bc375daca92135630e
MatchaTi <tehmatcha28@gmail.com>
2023-12-22 18:12:11 +0800

    🔥chore : add content pagination previous next neu

--- docs-content/Pagination/index.ts
+++ docs-content/Pagination/index.ts
@@ -1,5 +1,6 @@
 import { PAGINATIONBASIC } from "./source-code/paginationBasic";
 import { PAGINATIONNEU } from "./source-code/paginationNeu";
 import { PAGINATIONPREVIOUSNEXT } from "./source-code/paginationPreviousNext";
+import { PAGINATIONPREVIOUSNEXTNEU } from "./source-code/paginationPreviousNextNeu";
 
-export const COMPONENTS = [PAGINATIONBASIC,PAGINATIONNEU,PAGINATIONPREVIOUSNEXT]
+export const COMPONENTS = [PAGINATIONBASIC,PAGINATIONNEU,PAGINATIONPREVIOUSNEXT,PAGINATIONPREVIOUSNEXTNEU]
