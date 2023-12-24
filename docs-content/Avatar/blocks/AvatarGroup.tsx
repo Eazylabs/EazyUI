fd6d7132760260ac7fcb725416993981e2987a6c
MatchaTi <tehmatcha28@gmail.com>
2023-12-24 15:33:57 +0800

    🛠️fix : update style

--- docs-content/Avatar/blocks/AvatarGroup.tsx
+++ docs-content/Avatar/blocks/AvatarGroup.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import '../styles/style.css';
+import React from "react";
+import "../styles/style.css";
 
 export default function AvatarGroup() {
   return (
     <div className="avatar-group">
-        <img src='https://i.pravatar.cc/80' alt='avatar' />
-        <img src='https://i.pravatar.cc/80' alt='avatar' />
-        <img src='https://i.pravatar.cc/80' alt='avatar' />
-        <img src='https://i.pravatar.cc/80' alt='avatar' />
-        <img src='https://i.pravatar.cc/80' alt='avatar' />
+      <div className="avatar avatar-circle">
+        <img src="https://i.pravatar.cc/80" alt="avatar" />
+      </div>
+      <div className="avatar avatar-circle">
+        <img src="https://i.pravatar.cc/80" alt="avatar" />
+      </div>
+      <div className="avatar avatar-circle">
+        <img src="https://i.pravatar.cc/80" alt="avatar" />
+      </div>
+      <div className="avatar avatar-circle">
+        <img src="https://i.pravatar.cc/80" alt="avatar" />
+      </div>
+      <div className="avatar avatar-circle">
+        <img src="https://i.pravatar.cc/80" alt="avatar" />
+      </div>
+      <div className="avatar avatar-circle">
+        <img src="https://i.pravatar.cc/80" alt="avatar" />
+      </div>
     </div>
   );
 }
--- docs-content/Avatar/source-code/avatarGroup.ts
+++ docs-content/Avatar/source-code/avatarGroup.ts
@@ -2,41 +2,49 @@ import AvatarGroup from "../blocks/AvatarGroup";
 
 export const AVATARGROUP = {
   html: `<div class="avatar-group">
-        <img src='https://i.pravatar.cc/80' alt='avatar' />
-        <img src='https://i.pravatar.cc/80' alt='avatar' />
-        <img src='https://i.pravatar.cc/80' alt='avatar' />
-        <img src='https://i.pravatar.cc/80' alt='avatar' />
-        <img src='https://i.pravatar.cc/80' alt='avatar' />
-    </div>`,
-  css: `.avatar-group{
+  <div class="avatar avatar-circle">
+    <img src="https://i.pravatar.cc/80" alt="avatar" />
+  </div>
+  <div class="avatar avatar-circle">
+    <img src="https://i.pravatar.cc/80" alt="avatar" />
+  </div>
+  <div class="avatar avatar-circle">
+    <img src="https://i.pravatar.cc/80" alt="avatar" />
+  </div>
+  <div class="avatar avatar-circle">
+    <img src="https://i.pravatar.cc/80" alt="avatar" />
+  </div>
+  <div class="avatar avatar-circle">
+    <img src="https://i.pravatar.cc/80" alt="avatar" />
+  </div>
+  <div class="avatar avatar-circle">
+    <img src="https://i.pravatar.cc/80" alt="avatar" />
+  </div>
+</div>`,
+  css: `.avatar {
+  width: 80px;
+  height: 80px;
+  object-fit: cover;
+  background-color: gray;
+  overflow: hidden;
+}
+
+.avatar-circle {
+  border-radius: 50%;
+}
+
+.avatar-group{
   display: flex;
   align-items: center;
 }
 
-.avatar-group img{
-  border-radius: 50%;
-  width: 30px;
-  height: 30px;
+.avatar-group{
+  display: flex;
+  align-items: center;
 }
 
-.avatar-group img:nth-child(1) {
-  transform: translateX(10px);
-}
-
-.avatar-group img:nth-child(2) {
-  transform: translateX(0px);
-}
-
-.avatar-group img:nth-child(3) {
-  transform: translateX(-10px);
-}
-
-.avatar-group img:nth-child(4) {
-  transform: translateX(-20px);
-}
-
-.avatar-group img:nth-child(5) {
-  transform: translateX(-30px);
+.avatar-group .avatar:not(:first-child){
+  margin-left: -10px;
 }`,
   element: AvatarGroup,
 };
